c6a47403dc8abc0ede5fbc61c48e6d8ce4d7386c
JacobAntepli <70148209+JacobAntepli@users.noreply.github.com>
2024-01-30 22:32:29 -0500

    Header and empty defintions complete.

--- DragonFlyDev/Event.h
+++ DragonFlyDev/Event.h
@@ -1,1 +1,35 @@
-#pragma once
+#ifndef __EVENT_H__
+#define __EVENT_H__
+
+//Included resourses
+#include <string>
+
+
+using namespace std;
+namespace df {
+
+	class Event {
+
+	private:
+		//Default event type
+		const string UNDEFINED_EVENT = "df::undefined";
+
+		//Type of event
+		string m_event_type;
+	public:
+
+		//Constructer
+		Event();
+
+		//Deconstructer must be virtual
+		virtual ~Event();
+
+		//Set Event type
+		void setType(string new_type);
+
+		//Get Event type
+		string getType() const;
+
+	};//End of class
+}//End of namespace
+#endif // !__EVENT_H__
--- DragonFlyDev/test.cpp
+++ DragonFlyDev/test.cpp
@@ -3,7 +3,6 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
 #include <math.h>
-#include <string>
 
 #include "Manager.h"
 #include "LogManager.h"
